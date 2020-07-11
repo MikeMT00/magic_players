224fbb12e6c47d805109425beb7e0db02a800dcf
https://learn.co/MichaelThomas <michaelthomas0611@gmail.com>
2020-07-11 15:25:01 -0500

    Last Last Commit I think

--- lib/magic_players/players_api.rb
+++ lib/magic_players/players_api.rb
@@ -24,33 +24,6 @@ class PlayersAPI
 
     Players.new(firstName: player["firstName"], lastName: player["lastName"], yearsPro: player["yearsPro"], collegeName: player["collegeName"])
       end
-     
-
-    
   end
-
-
-  # def self.players_info_call
-  #   url = "https://api-nba-v1.p.rapidapi.com/players/teamId/26"
-
-  # #   http = Net::HTTP.new(url.host, url.port)
-  #   http.use_ssl = true
-  #   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  #   request = Net::HTTP::Get.new(url)
-  #   request["x-rapidapi-host"] = 'api-nba-v1.p.rapidapi.com'
-  #   request["x-rapidapi-key"] = '601b131cb1mshd49db1d37cdb4dep118f4fjsnde6b89799391'
-
-  #   response = http.request(request)
-  #   puts response.read_body
-  #   # url = "https://www.dnd5eapi.co/api/spells/#{players.index}"
-  #   # response = Net::HTTP.get(URI(url))
-  #   # players_details = JSON.parse(response)
-
-  #   players.firstName = players_first_name["firstName"]
-  #   players.lastName = players_last_name["lastName"]
-  #   players.yearsPro = players_years_pro["yearsPro"]
-  #   players.collegeName = players_college_name["collegeName"]
-  # end
 
 end
